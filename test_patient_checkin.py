a21e171f6d6e006405c0e90e8583cf63aa5e7bd3
aditya-varshneya <59046585+aditya-varshneya@users.noreply.github.com>
2020-06-16 12:44:54 +0530

    updated code and restructuring

--- test_patient_checkin.py
+++ test_patient_checkin.py
@@ -59,17 +59,27 @@ def test_chat():
     driver.find_element_by_xpath(
         "//*[@id='root']/div/div/div/div/section/div/div[3]/div/div/div[2]/div/div/div/div[3]/div/textarea").send_keys(
         "Hi Doctor,"
-        "I am wating for my consultation and uploaded history")
+        "I am waiting for my consultation and uploaded history")
     time.sleep(1)
-    driver.find_element_by_xpath(
+    try:
+        arrow_1 = driver.find_element_by_xpath(
         "//*[@id='root']/div/div/div/div/section/div/div[3]/div/div/div[2]/div/div/div/div[3]/div/button/span").click()
-    time.sleep(5)
-    driver.find_element_by_xpath("//*[@id='menu-close']/i").click()
-    time.sleep(5)
+        if arrow_1.is_enabled():
+            arrow_1.click()
+    except NoSuchElementException:
+        time.sleep(5)
+        driver.find_element_by_xpath("//*[@id='menu-close']/i").click()
+        time.sleep(5)
 
 
 def test_upload():
-    driver.find_element_by_xpath("//*[@id='root']/div/div/div/div/section/div/div[2]/div/div/div[1]/button").click()
+    try:
+        element_1 = driver.find_element_by_xpath("//*[@id='notificationmodal']/div/div/div[2]/div/div/div/button")
+        if element_1.is_displayed():
+            element_1.click()
+
+    except NoSuchElementException:
+        driver.find_element_by_xpath("//*[@id='root']/div/div/div/div/section/div/div[2]/div/div/div[1]/button").click()
     time.sleep(2)
     driver.find_element_by_xpath("//*[@id='#']/div/input").send_keys("C:/Users/Lenovo/Desktop/Python/file.png")
     driver.find_element_by_name("reportName").send_keys("Previous Prescription")

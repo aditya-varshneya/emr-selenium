98cd36640fdc3e99fed4c9b09c41306b0606ef40
aditya-varshneya <59046585+aditya-varshneya@users.noreply.github.com>
2020-07-05 20:26:39 +0530

    updated transfer feature

--- test_login_doc.py
+++ test_login_doc.py
@@ -26,7 +26,7 @@ def test_setup():
     chrome_options = webdriver.ChromeOptions()
     prefs = {"profile.default_content_setting_values.notifications": 1}
     chrome_options.add_experimental_option("prefs", prefs)
-    driver = webdriver.Chrome(chrome_options=chrome_options)
+    driver = webdriver.Chrome("C:\webdrivers\chromedriver.exe",chrome_options=chrome_options)
     driver.get(ip_url)
     driver.maximize_window()
 

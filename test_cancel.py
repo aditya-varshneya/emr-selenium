8df3cad2bfcbb8a740e2a360812f6e778eb762ad
aditya-varshneya <59046585+aditya-varshneya@users.noreply.github.com>
2020-07-10 20:29:44 +0530

    updated age with code optimization

--- test_cancel.py
+++ test_cancel.py
@@ -1,16 +1,10 @@
-from _ctypes import pointer
-from selenium import webdriver
-from selenium.webdriver.support.ui import Select
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 import time
-from datetime import date
-from selenium.webdriver import ChromeOptions
-import logging
-import pytest
 
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.wait import WebDriverWait
 
 # Parameters
 username = 8860879079
@@ -55,7 +49,7 @@ def test_doc_reg():
     driver.find_element_by_xpath("/html/body/div[3]/div/div/div[2]/div/div[2]/button").click()
     time.sleep(3)
     driver.find_element_by_id("name").send_keys(name)
-    driver.find_element_by_id("age").send_keys(age)
+    driver.find_element_by_id("age:no_of_years").send_keys(age)
     driver.find_element_by_id("phone").send_keys(phone)
     driver.find_element_by_xpath(
         "/html/body/div/div/div/div/div/div/div/div/div/div/div/form/div[6]/div[2]/div/div/div/div[1]/div/label/input").click()
@@ -70,16 +64,18 @@ def test_doc_reg():
     time.sleep(5)
 
 
-
 def test_cancellation():
-    driver.find_element_by_xpath("//*[@id='root']/div/div/div/div[1]/div/div/div[1]/div/div/div[1]/div/div[1]/div/div[2]").click()
+    driver.find_element_by_xpath(
+        "//*[@id='root']/div/div/div/div[1]/div/div/div[1]/div/div/div[1]/div/div[1]/div/div[2]").click()
     time.sleep(3)
-    driver.find_element_by_xpath("/html/body/div/div/div/div/div[1]/div/div/div[1]/div/div/div[2]/table/tbody/tr[1]/td[7]/button[1]").click()
+    driver.find_element_by_xpath(
+        "/html/body/div/div/div/div/div[1]/div/div/div[1]/div/div/div[2]/table/tbody/tr[1]/td[7]/button[1]").click()
     time.sleep(5)
     driver.find_element_by_xpath("//*[@id='root']/div/div/div/div/div/div[2]/div/div/div[2]/div/button[2]").click()
     time.sleep(2)
     driver.find_element_by_name("cancelRemarks").send_keys("Need to cancel")
-    driver.find_element_by_xpath("//*[@id='root']/div/div/div/div/div/div[2]/div/div/div[2]/section/div[2]/div/button").click()
+    driver.find_element_by_xpath(
+        "//*[@id='root']/div/div/div/div/div/div[2]/div/div/div[2]/section/div[2]/div/button").click()
     time.sleep(7)
     driver.find_element_by_xpath("//*[@id='root']/div/div/div/div[2]/div[2]/p/span[2]/button").click()
     time.sleep(7)
@@ -108,9 +104,8 @@ def test_refund():
     driver.find_element_by_xpath("/html/body/div[3]/div/div/div[2]/div/div[3]/button").click()
     time.sleep(5)
     driver.find_element_by_xpath(
-            "//*[@id='root']/div/div/div/div/div/div/div/div/div/div[3]/div[2]/p/span/button").click()
+        "//*[@id='root']/div/div/div/div/div/div/div/div/div/div[3]/div[2]/p/span/button").click()
     time.sleep(3)
     driver.back()
     time.sleep(3)
     driver.close()
-

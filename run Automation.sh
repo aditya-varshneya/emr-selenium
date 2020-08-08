d98088af740c176436fc0716c221a39629eccef6
aditya-varshneya <59046585+aditya-varshneya@users.noreply.github.com>
2020-08-08 13:13:41 +0530

    updated code

---
+++ run Automation.sh
@@ -0,0 +1,6 @@
+# shellcheck disable=SC2164
+cd C:\Users\Lenovo\Documents\GitHub\emr-selenium-automation
+pytest -v "test_appoint_patient.py" 
+pytest -v "test_cancel.py"
+pytest -v "test_login_doc.py"
+pytest -v "test_specific_scenario.py"
